28b717fa40aa5c3a5fd689553e3ec54d662c52b4
iamrishi09 <60710605+iamrishi09@users.noreply.github.com>
2020-09-01 17:32:38 +0530

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = props => (
+    <div>
+        <h1>Weather finder</h1>
+        <p>Findout weather and condition.</p>
+    </div>
+)
+export default Title;
